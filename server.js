e7d53c774cd716e1dfb83c9d9e0987881fc0b4b2
Ilya <ilya.yudin@metamins.com>
2023-03-28 10:02:10 +0500

    update

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("./dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.listen(PORT, () => {
-  console.log(`app listening on port ${PORT}!`);
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
